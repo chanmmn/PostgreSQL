6490582be2d2c81cb0218c85db9acd37f2c518a6
chanmmn <33271651+chanmmn@users.noreply.github.com>
2023-08-28 00:21:59 +0800

    Commit ConAppPostGreSQL
    Commit ConAppPostGreSQL

---
+++ ConAppPostGreSQL/ConAppPostGreSQL/Program.cs
@@ -0,0 +1,33 @@
+﻿using Npgsql;
+
+namespace ConAppPostGreSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Start!");
+            
+        }
+
+        public static void ReadPostGreSQL()
+        {
+            string cs = "Host=localhost;Username=postgres;Password=Password;Database=test";
+
+            using NpgsqlConnection con = new NpgsqlConnection(cs);
+            con.Open();
+
+            string sql = "SELECT * From Person";
+
+            using NpgsqlCommand cmd = new NpgsqlCommand(sql, con);
+
+            //var version = cmd.ExecuteScalar().ToString();
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0].ToString());
+            }
+            //Console.WriteLine($"PostgreSQL version: {version}");
+        }
+    }
+}
