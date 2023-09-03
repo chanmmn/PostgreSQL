40ac9d7f974fe3bb2caee65bd6456a250426d761
Ming Man <chanmmn@hotmail.com>
2023-09-03 16:30:19 +0800

    Commit ConppPostgreSQLEF1Insert
    Commit ConppPostgreSQLEF1Insert

---
+++ ConppPostgreSQLEF1Insert/ConppPostgreSQLEF1Insert/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace ConppPostgreSQLEF1Insert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Start!");
+            ReadPostgreSQL();
+            InsertData();
+        }
+        public static void ReadPostgreSQL()
+        {
+            TestContext context = new TestContext();
+            var result = from r in context.People select r;
+            foreach (Person r in result)
+            {
+                Console.WriteLine(r.FirstName); Console.WriteLine(r.LastName);
+            }
+        }
+        public static void InsertData()
+        {
+            TestContext context = new TestContext();
+            Person person = new Person();
+            person.Id = 10;
+            person.FirstName = "Yuk";
+            person.LastName = "Ki";
+            context.People.Add(person);
+            context.SaveChanges();
+        }
+
+
+    }
+}
